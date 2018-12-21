bc169e6a5263a2546c149ba00dc930f5dff633ee
luc <luctunechi45@gmail.com>
2018-12-21 17:13:23 +0200

    Questioner pages

--- ui/js/script.js
+++ ui/js/script.js
@@ -6,7 +6,7 @@ var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-  
+
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -15,7 +15,7 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+} 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
