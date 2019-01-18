8bb81f629883bf6695f310660416897dbcdc761a
luc <luctunechi45@gmail.com>
2019-01-18 19:53:02 +0200

    update readme

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/home', (req, res) => {
   res.render('Welcome to questioner app');
 });
 
