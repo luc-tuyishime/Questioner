5d6465727a5f30990e92a18dc47d3abb8b164852
luc <luctunechi45@gmail.com>
2019-01-19 17:24:34 +0200

    [ch-163344099] validate the Rsvp endpoint

--- routes/rsvpMeetup.js
+++ routes/rsvpMeetup.js
@@ -10,6 +10,12 @@ const router = express.Router();
 
 router.post('/:id/rsvps', (req, res) => {
   const meetup = meetups.find(m => m.id === parseInt(req.params.id, 10));
+  if (!meetup) {
+    return res.status(404).send({
+      status: 404,
+      error: `The meetup with the id ${req.params.id} was not found`
+    });
+  }
   console.log(parseInt(req.params.id, 10));
   if (!meetup) {
     return res.status(404).send({
