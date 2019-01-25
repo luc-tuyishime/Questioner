bb3a0aa38d94e003f8e6a3cc7bc4a457d93574cc
luc <luctunechi45@gmail.com>
2019-01-25 08:37:25 +0200

    [ch-163385912] add sign up with token

--- controllers/users.js
+++ controllers/users.js
@@ -43,54 +43,55 @@ const User = {
       }
       return res.status(400).send(error);
     }
-  },
+  }
+}
   /**
    * Login
    * @param {object} req
    * @param {object} res
    * @returns {object} user object
    */
-  async login(req, res) {
-      const hashPassword = Helper.hashPassword(req.body.password);
-    if (!req.body.email || !req.body.password) {
-      return res.status(400).send({'message': 'Some values are missing'});
-    }
-    if (!Helper.isValidEmail(req.body.email)) {
-      return res.status(400).send({ 'message': 'Please enter a valid email address' });
-    }
-    const text = 'SELECT * FROM users WHERE email = $1';
-    try {
-      const { rows } = await db.query(text, [req.body.email]);
-      if (!rows[0]) {
-        return res.status(400).send({'message': 'The credentials you provided is incorrect'});
-      }
-      if(!Helper.comparePassword(rows[0].password, hashPassword)) {
-        return res.status(400).send({ 'message': 'The credentials you provided is incorrect' });
-      }
-      const token = Helper.generateToken(rows[0].id);
-      return res.status(200).send({ token });
-    } catch(error) {
-      return res.status(400).send(error)
-    }
-  },
+  // async login(req, res) {
+  //     const hashPassword = Helper.hashPassword(req.body.password);
+  //   if (!req.body.email || !req.body.password) {
+  //     return res.status(400).send({'message': 'Some values are missing'});
+  //   }
+  //   if (!Helper.isValidEmail(req.body.email)) {
+  //     return res.status(400).send({ 'message': 'Please enter a valid email address' });
+  //   }
+  //   const text = 'SELECT * FROM users WHERE email = $1';
+  //   try {
+  //     const { rows } = await db.query(text, [req.body.email]);
+  //     if (!rows[0]) {
+  //       return res.status(400).send({'message': 'The credentials you provided is incorrect'});
+  //     }
+  //     if(!Helper.comparePassword(rows[0].password, hashPassword)) {
+  //       return res.status(400).send({ 'message': 'The credentials you provided is incorrect' });
+  //     }
+  //     const token = Helper.generateToken(rows[0].id);
+  //     return res.status(200).send({ token });
+  //   } catch(error) {
+  //     return res.status(400).send(error)
+  //   }
+  // },
   /**
    * Delete A User
    * @param {object} req
    * @param {object} res
    * @returns {void} return status code 204
    */
-  async delete(req, res) {
-    const deleteQuery = 'DELETE FROM users WHERE id=$1 returning *';
-    try {
-      const { rows } = await db.query(deleteQuery, [req.user.id]);
-      if(!rows[0]) {
-        return res.status(404).send({'message': 'user not found'});
-      }
-      return res.status(204).send({ 'message': 'deleted' });
-    } catch(error) {
-      return res.status(400).send(error);
-    }
-  }
-}
+//   async delete(req, res) {
+//     const deleteQuery = 'DELETE FROM users WHERE id=$1 returning *';
+//     try {
+//       const { rows } = await db.query(deleteQuery, [req.user.id]);
+//       if(!rows[0]) {
+//         return res.status(404).send({'message': 'user not found'});
+//       }
+//       return res.status(204).send({ 'message': 'deleted' });
+//     } catch(error) {
+//       return res.status(400).send(error);
+//     }
+//   }
+// }
 
 export default User;
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ import Auth from '../middleware/auth';
 const router = express.Router();
 
 router.post("/create", userDb.create);
-router.post("/login", userDb.login);
+// router.post("/login", userDb.login);
 // router.get("/:id", userController.fetch);
 // router.patch("/:id", userController.update);
-router.delete("/:id", Auth.verifyToken, userDb.delete);
+// router.delete("/:id", Auth.verifyToken, userDb.delete);
 
 module.exports = router;
